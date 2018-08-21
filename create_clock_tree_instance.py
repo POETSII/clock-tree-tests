c42c04b94551a40bbe5844b74abaf8da7dd0fc4e
Jonny Beaumont <jonathan.r.beaumont@gmail.com>
2018-08-21 13:28:42 +0100

    Generating an instance now copies the given graph type.

--- create_clock_tree_instance.py
+++ create_clock_tree_instance.py
@@ -3,11 +3,12 @@
 from graph.core import *
 
 from graph.load_xml import load_graph_types_and_instances
-from graph.save_xml_stream import save_graph
+from graph.save_xml_stream import write_device_instance, write_edge_instance
 import sys
 import os
 import math
 import csv
+import json
 
 import os
 appBase=os.path.dirname(os.path.realpath(__file__))
@@ -56,6 +57,45 @@ def create(res, rootPrefix, rootParent, drainPrefix, drainParent, depth):
 
     return node
 
+def save_graph(dst, graph):
+    if isinstance(dst, str):
+        with open(dst,"wt") as dstFile:
+            save_graph(dstFile, graph)
+    else:
+        # Copy Graph Type from given file
+        with open(src) as fp:
+            lines = fp.read().split("\n")
+
+        for l in lines:
+            if not (l == '</Graphs>'):
+                dst.write(l)
+                dst.write('\n')
+
+        # Graph Instance
+        dst.write(' <GraphInstance id="{}" graphTypeId="{}">\n'.format(graph.id,graph.graph_type.id))
+        if graph.properties:
+            dst.write('   <Properties>\n')
+            dst.write(json.dumps(graph.properties,indent=2)[1:-1])
+            dst.write('\n')
+            dst.write('   </Properties>\n')
+        if graph.metadata:
+            dst.write('   <MetaData>\n')
+            dst.write(json.dumps(graph.metadata,indent=2)[1:-1])
+            dst.write('\n')
+            dst.write('   </MetaData>\n')
+        dst.write('  <DeviceInstances>\n')
+        for di in graph.device_instances.values():
+            write_device_instance(dst,di)
+        dst.write('  </DeviceInstances>\n')
+        dst.write('  <EdgeInstances>\n')
+        for ei in graph.edge_instances.values():
+            write_edge_instance(dst,ei)
+        dst.write('  </EdgeInstances>\n')
+        dst.write(' </GraphInstance>\n')
+
+        # Close Grpah declaration
+        dst.write('</Graphs>\n')
+
 def createInstance(filepath):
     instName="clock_{}_{}".format(d,b)
     res=GraphInstance(instName, graphType, properties)
@@ -69,10 +109,15 @@ def createInstance(filepath):
 
     create(res, "root",root, "drain", drain ,d)
 
-    if (filepath == ""):
-        save_graph(res,sys.stdout)
+    if (filepath == ''):
+        save_graph(sys.stdout, res)
     else:
-        save_graph(res,filepath)
+        if os.path.exists(filepath):
+            try:
+                os.remove(filepath)
+            except OSError, e:
+                print ("Error: %s - %s." % (e.filename,e.strerror))
+        save_graph(filepath, res)
 
 if len(sys.argv)==2:
     csvfile=sys.argv[1]
